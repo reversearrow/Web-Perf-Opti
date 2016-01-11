75914867b7a2f58e8f8c9fbdcbea42189067e4c8
reversearrow <reversearrow@gmail.com>
2016-01-10 20:22:44 -0500

    Image Minified using Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,11 +23,22 @@ module.exports = function(grunt) {
       ext: '.min.css'
     }]
   }
+},
+imagemin: {                          // Task
+    dynamic: {                         // Another target
+      files: [{
+        expand: true,                  // Enable dynamic expansion
+        cwd: 'src/',                   // Src matches are relative to this path
+        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+        dest: 'dist/img/'                  // Destination path prefix
+      }]
+      }
+
 }
 
 });
 
-grunt.registerTask('default', ['htmlmin','cssmin']);
+grunt.registerTask('default', ['htmlmin','cssmin','imagemin']);
 
 };
 
