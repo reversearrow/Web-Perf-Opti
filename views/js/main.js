6164ca5b8609045034c8a5da0fe0af5d27bf6830
reversearrow <reversearrow@gmail.com>
2016-01-18 21:01:31 -0500

    Removed Forced Synchronized Layout and re-size is below 5ms from 100ms

--- views/js/main.js
+++ views/js/main.js
@@ -450,9 +450,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[0], size);
+    var offset = document.querySelectorAll(".randomPizzaContainer")[0].offsetWidth;
+    var newwidth = (offset + dx) + 'px'
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
